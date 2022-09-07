1eaa4a0bef1159d80d8b8ed0fea7f0bec52f6c0b
Charan Yadav <charan.yadav@cision.com>
2022-09-07 01:16:57 -0700

    added icons for sorting

--- src/component/TableHead.js
+++ src/component/TableHead.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import Table from "./Table";
+import {AiOutlineUp, AiOutlineDown } from 'react-icons/ai';
+
 
 const TableHead = ({ columns, data }) => {
   const sorting = (accessor, tabledata) => {
     console.log("test1",tabledata, accessor )
     if(accessor === "FeedName")
-    tabledata.sort((a, b) => a.feedname.toLowerCase() < b.feedname.toLowerCase() ? -1 : 1);
+    return tabledata.sort((a, b) => a.feedname.toLowerCase() < b.feedname.toLowerCase() ? -1 : 1);
     else if(accessor === "FeedDate")
-    tabledata.sort((a, b) => a.date.toLowerCase() < b.date.toLowerCase() ? -1 : 1);
+    return tabledata.sort((a, b) => a.date.toLowerCase() < b.date.toLowerCase() ? -1 : 1);
     else if(accessor === "FeedCount")
-    tabledata.sort((a, b) => a.feedcount - b.feedcount)
-    console.log("test2",tabledata )
+    return tabledata.sort((a, b) => a.feedcount - b.feedcount)
+    //console.log("test2",tabledata )
   }
   const handleSortingChange = (accessor, tableData) => {
     
@@ -21,8 +23,9 @@ const TableHead = ({ columns, data }) => {
       tabledata.push(x);
     })
     })
-    return sorting(accessor, tabledata);
+    sorting(accessor, tabledata);
     //Table("", tabledata,columns);
+    console.log("test2",tabledata )
    };
 
   return (
@@ -31,9 +34,16 @@ const TableHead = ({ columns, data }) => {
         {columns.map(({ label, key }) => {
           
           return (
-            <th key={label} onClick={() => handleSortingChange(label, data)}>
-              {label}
-            </th>
+            //<th key={label} onClick={() => handleSortingChange(label, data)}>
+             // {label}
+           // </th>
+            
+            <th key={label}> 
+            <div className='th-sorting'>{label} 
+            <div className='th-sort-icon'>
+            <AiOutlineUp onClick={() => handleSortingChange(label, data)} className='srt-icon' />
+            <AiOutlineDown onClick className='srt-icon' />
+            </div></div></th>
           );
         })}
       </tr>
